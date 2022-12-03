106adf4f06c40d74e632ecfaef09a23f7644068c
Felipe Bambirra <lipex360@gmail.com>
2022-12-03 00:14:25 -0300

    main configs added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './core/helpers/database/database.module';
-import { HealthModule } from './core/helpers/healthcheck/healthcheck.module';
+import { DatabaseModule } from './shared/configs/database/database.module';
+import { HealthModule } from './shared/helpers/healthcheck/healthcheck.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule.register(), HealthModule],
